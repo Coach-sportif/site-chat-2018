19d6999b44bae6b54566cac315451a4df898986f
Coach-sportif <kevin.battle@hotmail.fr>
2018-02-28 15:15:18 +0100

    debut du site

---
+++ js/script.js
@@ -0,0 +1,74 @@
+/*GESTION MENU*/
+var header = document.querySelector('.header');
+var hauteurHeader = header.offsetHeight;
+var menu = document.querySelectorAll('.menu');
+var bar = document.querySelector('.bar');
+var actif = document.querySelector('.actif');
+var ensemble = document.querySelector('.ensemble');
+
+
+// -fonction init
+//-enlever et rajouter la classe actif
+//-rajouter un ev onclick sur les items du menu
+//-animation de la barre  (largeur et decalage partie gauche)
+
+var section = document.querySelectorAll('.section');
+var sectionActif = section[0];
+var itemsOld = 0;
+var sectionCopy;
+var sectionCopy2;
+var peutClick = true;
+
+function gestion(){
+  if(this === actif || peutClick === false) return;
+  peutClick = false;
+  actif.classList.remove('actif');
+  this.classList.add('actif');
+  actif = this;
+  lancement();
+  //Partie de gestion du deplacement des sections
+  var itemsNew = this.dataset.i;
+
+  itemsOld = parseInt(itemsNew);
+  sectionActif = section[`${itemsOld}`];
+  ensemble.removeChild(sectionActif);
+  var sectionActuel = document.querySelectorAll('.section');
+  ensemble.insertBefore(sectionActif, sectionActuel[1]);
+  window.setTimeout(() => {
+    sectionActif.classList.add('deplacementSection')
+  }, 10);
+  sectionCopy = sectionActuel[0];
+  window.setTimeout( () => {
+    ensemble.removeChild(sectionActuel[0]);
+    ensemble.appendChild(sectionCopy);
+    sectionActif.classList.remove('deplacementSection');
+    peutClick = true;
+  }, 600);
+}
+
+function click(){
+  for(var i = 0; i < menu.length; i++){
+    menu[i].addEventListener('click', gestion);
+  }
+}
+
+function lancement(){
+  var largeur = actif.offsetWidth;
+  var position = actif.offsetLeft;
+  bar.style.transform =
+   `translate3d(${position}px, 0, 0)`;
+  bar.style.width = `${largeur}px`;
+}
+
+var time;
+
+function init(){
+  click();
+  lancement();
+  window.addEventListener('resize', () => {
+    clearTimeout(time);
+    time = setTimeout(lancement, 30);
+  });
+}
+
+init();
