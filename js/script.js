2fdd8537451bba213a3980023b1e51f70f1c58de
Coach-sportif <kevin.battle@hotmail.fr>
2018-03-03 11:49:10 +0100

    rajout partie2 et 3 partiellement

--- js/script.js
+++ js/script.js
@@ -95,3 +95,42 @@ function tourneLaCarteDansLeBonSens() {
 lesBoutonsDesCartes.forEach(boutton => {
   boutton.addEventListener('click', tourneLaCarteDansLeBonSens);
 });
+
+// PARTIE2 ET PARTIE 3 ANIMATION DIV
+// Je recupère les listes des boutons
+var buttonChats = document.querySelectorAll('.buttonChats');
+var carteInfos = document.querySelectorAll('.info');
+var close = document.querySelectorAll('.close');
+
+// Les index pour les for
+var i = 0;
+var j = 0;
+var k = 0;
+
+// Boucle qui permet d'ajouter l'action de montrer la carte info correspondant
+// au bouton cliqué
+for(i = 0; i < buttonChats.length; i++) {
+  // On ajoute l'evènement à tous les boutons
+  buttonChats[i].onclick = function fullscreen() {
+    // this = le bouton cliqué
+    // dataset.info permet de recupérer la valeur qu'on attrivuer dans data-info
+    // pour chaque bouton
+    // ce qui nous permet de reconstruire la classe que l'on cherche
+    var nomDeLaClasseRecherchee = '.info' + this.dataset.info; // ex: .info1, .info2, .info3
+    // On cherche la carte info qui correspond à la classe générée
+    var laCarteInfo = document.querySelector(nomDeLaClasseRecherchee);
+    // On lui applique la classe active pour pouvoir l'afficher
+    laCarteInfo.classList.add('active');
+  }
+}
+
+// On recupère les bouton pour fermer la carte
+// On supprime la classe active à son parent pour la fermer
+for(k = 0; k < close.length; k++) {
+  close[k].onclick = function closeCarteInfo() {
+    this.parentNode.classList.remove('active');
+  }
+}
+
+// close.style.top = `calc(${hauteurHeader}px + 20px)`;
+//VOIR AVEC DEXTER COMME RAJOUTER UNE PROPRIETE DANS LE CSS ET PAS DANS UN ELEMENT HTML
